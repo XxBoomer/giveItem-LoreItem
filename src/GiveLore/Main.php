33954858c5c7c25a9713f71dc8ecbfa4b200b6e7
CentOsDogE <XxBoomer@users.noreply.github.com>
2017-12-17 13:54:11 +0700

    Update Main.php

--- src/GiveLore/Main.php
+++ src/GiveLore/Main.php
@@ -9,26 +9,27 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\Command\Command;
 use pocketmine\Command\CommandSender;
+
 class Main extends PluginBase{
 	
 	public function onEnable(){
-		$this->getServer()->getLogger()->info("BasicCommands enabled!");
+		$this->getServer()->getLogger()->info("GiveLore enabled!");
 	}
 	
 	public function onDisable(){
-		$this->getServer()->getLogger()->info("BasicCommands disabled!");
+		$this->getServer()->getLogger()->info("GiveLore disabled!");
 	}
 	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		
 		switch($cmd->getName()){
 			
-			case "GiveLore":
-       if($sender->hasPermission("GiveLore.use")} {
+			case "givelore":
+       if($sender->hasPermission("givelore.use")} {
 				$item = Item::get(264,0,1);
         $item->setCustomName("§bSiêu kim cương");
         $item->setLore(["§b▁§c▂§a▃§e▄§d▅§9▆§6▇ §f§lSUPER DIAMOND§r §6▇§9▆§d▅§e▄§a▃§c▂§b▁", "§bCục kim cương này éo có td j hết :3"]);
-        if(!$sender->hasPermission("GiveLore.use")} {
+        if(!$sender->hasPermission("givelore.use")} {
           $sender->sendMessage("§cTrình loz để lấy kc :3");
 			  }
 			break;
